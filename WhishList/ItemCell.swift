75bf31a6f8f5796830b5a34e340ba11c0cec1b77
aivars <aivars.meijers@gmail.com>
2016-09-21 05:56:33 +0300

    UI finished
    Table views and cell updated

---
+++ WhishList/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  WhishList
+//
+//  Created by Aivars Meijers on 21.09.16.
+//  Copyright © 2016. g. Aivars Meijers. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+ 
+}
