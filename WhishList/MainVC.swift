9339863871d15f11aee534ed2b47c6a11e47166b
aivars <aivars.meijers@gmail.com>
2016-09-21 05:38:58 +0300

    UI setup updated

--- WhishList/MainVC.swift
+++ WhishList/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  WhishList
 //
 //  Created by Aivars Meijers on 20.09.16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
